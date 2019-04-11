3111fd23a737d977afe336587556c48dd0bc2bf9
seejaym <48082282+seejaym@users.noreply.github.com>
2019-04-11 20:02:19 +1000

    rotation enc/dec work
    added comments

--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
 int main(){
-	char message[100000], characters;		
-        int i, userdefinedkey, tasknumber;
-	printf("Enter Number For Corresponding Task\n");
-	printf("Task 1: Rotation Cipher Encrption\n");
-	printf("Task 2: Rotation Cipher Decryption\n");
-	printf("Enter Task Number Here: ");
-	scanf("%d", &tasknumber);
-	stdin = freopen(NULL,"r",stdin); //resets stdin, without this the switch case will not work properly
+	//int a=0,b=1,c=2,d=3,e=4,f=5,g=6,h=7,i=8,j=9,k=10,l=11,m=12,n=13,o=14,p=15,q=16,r=17,s=18,t=19,u=20,v=21,w=22,x=23,y=24,z=25;
+	//int A=26,B=27,C=28,D=29,E=30,F=31,G=32,H=33,I=34,J=35,K=36,L=37,M=38,N=39,O=40,P=41,Q=42,R=43,S=44,T=45,U=46,V=47,W=48,X=49,Y=50,Z=51;
+	char message[100000], characters;			//declares message array, characters	
+        int i, userdefinedkey, tasknumber;			//declare integers i, userdefinedkey, tasknumber
+	printf("Enter Number For Corresponding Task\n");	//display message for user to enter corresponding task
+	printf("Task 1: Rotation Cipher Encrption\n");		//display message for rotation cipher 1 encryption
+	printf("Task 2: Rotation Cipher Decryption\n");		//display message for rotation cipher 2 decyption
+	printf("Task 3: Substitution Cipher Encryption\n");	//display message for substition cipher encryption
+	printf("Task 4: Substitution Cipher Decryption\n");	//display message for cipher decrytion
+	printf("Task 5: Rotation Cipher Encryption, Text Only\n");//display message for cipher encryption, text only provided
+	printf("Task 6: Substitution Cipher Decryption, Text Only\n");//display message for cipher decryption, text only provided
+	printf("Exit 7: Exit Program\n");			//display message for exit condition of program
+	printf("Enter Task Number Here: ");			//prints message to enter in task number
+	scanf("%d", &tasknumber);				//scans for task number entered
+	if (tasknumber < 1 || tasknumber > 7)			//checks if number entered is less than 1 or greater than 7
+		{return main();}				//loops back to main if number is out of range
+	stdin = freopen(NULL,"r",stdin); 			//resets stdin(memory), without this the switch case will not work properly
 	switch(tasknumber){
 		case 1:
 			printf("Enter a message to encrypt: ");
@@ -19,22 +28,26 @@ int main(){
 			for (i = 0; message[i] != '\0'; i++){
 	   			characters = message[i];
 				if (characters >= 'a' && characters <= 'z'){
-	   				characters = characters + userdefinedkey;
+					characters = characters - 97;
+	   				characters = (characters + userdefinedkey)%26;
 	  					if (characters > 'z'){
 	  						characters = characters - 'z' + 'a' - 1;
 						}
-
+					characters = characters + 97;
 					message[i] = characters;
 				}
 				else if(characters >= 'A' && characters <= 'Z'){
-						characters = characters + userdefinedkey;
+					characters = characters - 65;
+					characters = (characters + userdefinedkey)%26;
 							if (characters > 'Z'){
 								characters = characters - 'Z' + 'A' - 1;
 							}
+					characters = characters + 65;
 					message[i] = characters;
 				}
        			}
        			printf("Encrypted message: %s \n", message);
+			return main();
        		break;
        		case 2:
 			printf("Enter a message to decrypt: ");
@@ -45,24 +58,37 @@ int main(){
 			for (i = 0; message[i] != '\0'; i++){
 	   			characters = message[i];
 				if (characters >= 'a' && characters <= 'z'){
-	   				characters = characters - userdefinedkey;
-	  					if (characters > 'z'){
-	  						characters = characters - 'z' - 'a' + 1;
+					characters = characters - 97;
+	   				characters = (characters - userdefinedkey)%26;
+	  					if (characters < 'a'){
+	  						characters = characters + 'z' - 'a' + 1;
 						}
-
+					characters = characters + 97;
 					message[i] = characters;
 				}
 				else if(characters >= 'A' && characters <= 'Z'){
-						characters = characters - userdefinedkey;
-							if (characters > 'Z'){
+					characters = characters - 65;
+						characters = (characters - userdefinedkey)%26;
+							if (characters < 'A'){
 								characters = characters + 'Z' - 'A' + 1;
 							}
+					characters = characters + 65;
 					message[i] = characters;
 				}
        			}
-       			printf("dEcrypted message: %s \n", message);
+       			printf("decrypted message: %s \n", message);
+			return main();
        		break;					
-			
+		case 3:
+		break;
+		case 4:
+		break;
+		case 5:
+		break;
+		case 6:
+		break;
+		case 7: return 0;
+		break;	
 		default: printf("enter a number\n");
 	}
 } 
